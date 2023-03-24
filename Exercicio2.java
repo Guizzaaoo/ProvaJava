8891435644d800a0fc390a9bfdf9e2ec94f25100
ChristopherM51 <christophermatheus1@hotmail.com>
2023-03-23 21:14:37 -0300

    Prova

---
+++ Exercicio2.java
@@ -0,0 +1,14 @@
+package JavaProva;
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+    public void Corleone() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o Numero da MAtricula do aluno:");
+        int matricula = sc.nextInt();
+
+        String[] times = {"Time Chris", "time Greg", "time Caruso", "time Jerome"};
+    }
+}
---
+++ Exercicio3.java
@@ -0,0 +1,21 @@
+package JavaProva;
+
+public class Exercicio3 {
+    public static void main(String[] args) {
+        double kg = 0;
+        double kgMorango = 2.50;
+        double kgmaca = 1.80;
+        double kgBanana = 1.30;
+        double percentualdesconto = 0.0;
+        if (kg < 8){
+            percentualdesconto = 10;
+        }
+double desconto = (kgMorango * percentualdesconto) / 100;
+double desconto = (kgmaca * percentualdesconto) / 100;
+double desconto = (kgBanana * percentualdesconto) / 100;
+double total = (kgMorango + kgmaca + kgBanana + desconto)
+if(total > 25.00){
+    total = percentualdesconto / 100;
+}
+    }
+}
