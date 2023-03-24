4d2b799ae75f8821a464ad3ea3fd9fa2b4fce52f
Guizzaaoo <123784155+Guizzaaoo@users.noreply.github.com>
2023-03-23 21:41:48 -0300

    Update Exercicio3.java

--- Exercicio3.java
+++ Exercicio3.java
@@ -1,21 +1,34 @@
 package JavaProva;
 
 public class Exercicio3 {
-    public static void main(String[] args) {
-        double kg = 0;
-        double kgMorango = 2.50;
-        double kgmaca = 1.80;
-        double kgBanana = 1.30;
-        double percentualdesconto = 0.0;
-        if (kg < 8){
-            percentualdesconto = 10;
-        }
-double desconto = (kgMorango * percentualdesconto) / 100;
-double desconto = (kgmaca * percentualdesconto) / 100;
-double desconto = (kgBanana * percentualdesconto) / 100;
-double total = (kgMorango + kgmaca + kgBanana + desconto)
-if(total > 25.00){
-    total = percentualdesconto / 100;
-}
+    public void Exercicio3() {
+        Scanner input = new Scanner(System.in);
     }
-}
+     // Ler a quantidade (em Kg) de morangos adquiridos
+     System.out.println("Quantidade (em Kg) de morangos adquiridos: ");
+     double Morangos = input.nextDouble();
+
+     // Ler a quantidade (em Kg) de maças adquiridas
+     System.out.print("Quantidade (em Kg) de maças adquiridas: ");
+     double Macas = input.nextDouble();
+
+     // Ler a quantidade (em Kg) de bananas adquiridas
+     System.out.print("Quantidade (em Kg) de bananas adquiridas: ");
+     double Bananas = input.nextDouble();
+
+     // Calcular o valor total da compra
+     double valorTotal = (Morangos * 2.50) + (Macas * 1.80) + (Bananas * 1.30);
+
+     // Verificar se o valor total da compra ultrapassa R$ 25,00 ou se a quantidade total de frutas adquiridas é maior que 8 Kg
+     if (valorTotal > 25 || (Morangos + Macas + Bananas) > 8) {
+         // Calcular o desconto de 10% sobre o valor total da compra
+         double desconto = valorTotal * 0.1;
+         valorTotal -= desconto;
+     }
+
+     // Exibir o valor a ser pago pelo cliente, considerando o desconto calculado no passo 7 (ou o valor total da compra, caso não haja desconto)
+     System.out.println("Valor total da compra: R$ " + valorTotal);
+
+     input.close();
+ }
+
